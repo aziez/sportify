25e5555eabbd7c2a4d2e59b2d30947f034d60c04
Aziez <aziz13.id@gmail.com>
2024-07-01 08:36:44 +0700

    fixed talwind color fatlern

---
+++ tailwindcss.d.ts
@@ -0,0 +1,5 @@
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+  // You can specify the types if you know them, or leave it as `any`.
+  const flattenColorPalette: any;
+  export default flattenColorPalette;
+}
